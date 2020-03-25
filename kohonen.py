41902a51405de1670e6be3c8fcfb0fde06e65442
Marcos Sombra <marcos.sombraaa@gmail.com>
2020-03-25 15:30:20 -0300

    predict

--- kohonen.py
+++ kohonen.py
@@ -1,6 +1,7 @@
 from random import random
 from scipy.spatial import distance
 import numpy as np
+import math
 
 
 def normalize(v):
@@ -41,6 +42,7 @@ class Kohonen:
         # Contador de épocas = 0
         epochs = 0
         while epochs < max_epochs:
+            is_done = False
             # para cada entrada xi no conjunto de entradas, faça:
             for xi in X:
                 # normalizando entrada
@@ -48,17 +50,54 @@ class Kohonen:
                 # variaveis para indicar o 'neuronio' mais próximo e a distancia
                 nearest = None
                 lowest_distance = float('inf')
+                neighbors = []
                 # checando a distancia entre xi
-                for neuron in self.__neurons:
+                n = len(self.__neurons)
+                raiz_n = int(math.sqrt(n))
+                for i in range(n):
+                    neuron = self.__neurons[i]
                     current_distance = neuron.distance_to_input(xi)
                     if current_distance < lowest_distance:
                         lowest_distance = current_distance
                         nearest = neuron
+                        neighbors = []
+                        if i - 1 > -1:
+                            neighbors.append(i - 1)
+                        if i - raiz_n > -1:
+                            neighbors.append(i - 1)
+                        if i + 1 < n:
+                            neighbors.append(i - 1)
+                        if i + raiz_n < n:
+                            neighbors.append(i - 1)
                 
                 # ajuste de pesos
                 ajuste = (self.__learn_ratio * lowest_distance)
+                if ajuste < 0.0001:
+                    is_done = True
                 nearest.weights = nearest.weights + ajuste
+                # ajuste de pesos dos vizinhos
+                for n in neighbors:
+                    n = self.__neurons[n]
+                    ajuste = ((self.__learn_ratio / 2) * lowest_distance)
+                    n.weights = n.weights + ajuste
             epochs += 1
+            if is_done:
+                break
 
-    def predict(self, x):
-        pass
+    def predict(self, X):
+        respostas = []
+        # para cada entrada xi no conjunto de entradas, faça:
+        for xi in X:
+            # normalizando entrada
+            xi = normalize(xi)
+            # variaveis para indicar o 'neuronio' mais próximo e a distancia
+            nearest = None
+            lowest_distance = float('inf')
+            # checando a distancia entre xi
+            for neuron in self.__neurons:
+                current_distance = neuron.distance_to_input(xi)
+                if current_distance < lowest_distance:
+                    lowest_distance = current_distance
+                    nearest = neuron
+            respostas.append(nearest)
+        return respostas
